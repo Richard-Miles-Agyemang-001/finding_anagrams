7f8b00f27ffdf1e5d8825ab2fe33e80a9a1327f9
richard-miles-agyemang-001 <richardayemang601@gmail.com>
2022-06-11 13:56:00 +0000

    final commit

--- main.py
+++ main.py
@@ -6,6 +6,17 @@
 
 def find_anagram(word, anagram):
     # [assignment] Add your code here
+    #change words to lowercase
+    word = word.lower()
+    anagram = anagram.lower()
 
-    return True
+    # remove whitespace
+    word = word.replace(" ", "")
+    anagram = anagram.replace(" ", "")
+
+    #sorted(word) == sorted(anagram) True or  False
+    return sorted(word) == sorted(anagram)
+
+print("calling find_anagram with 'hello' and 'check' ", find_anagram("hello", "check"))
+print("calling find_anagram with 'below' and 'elbow' ", find_anagram("below", "elbow"))
 
